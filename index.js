c5380a49577a6ed697908b4223bc49911c2f2213
pashaman <plavel@gmail.com>
2019-06-24 10:57:28 +0300

    v2.0.0

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { RednomicUnit } from "./lib/RednomicUnit";
 import { RednomicUnitAdapter } from "./lib/RednomicUnitAdapter";
 import { RednomicUnitConnector} from "./lib/RednomicUnitConnector";
+import { RednomicUnitGroup} from "./lib/RednomicUnitGroup";
 import { RednomicQueueId } from "./lib/RednomicQueueId";
 import { RednomicServer } from "./lib/RednomicServer";
 
 export {
   RednomicUnit,
+  RednomicUnitGroup,
   RednomicUnitAdapter,
   RednomicUnitConnector,
   RednomicQueueId,
